28de1233bd37c4573580c4f95acb26139e3c1c0a
AlMamunTalukder <almamuntalukder0@gmail.com>
2025-09-02 13:24:29 +0600

    sove

--- src/app/modules/auth/auth.utils.ts
+++ src/app/modules/auth/auth.utils.ts
@@ -10,7 +10,6 @@ export const createToken = (
   return jwt.sign(jwtPayload, secret, options);
 };
 
-// /* eslint-disable @typescript-eslint/no-explicit-any */
 // import jwt from 'jsonwebtoken';
 // export const createToken = (
 //   jwtPayload: { email: any; role: any },
